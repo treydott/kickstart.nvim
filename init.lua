134e78aa1b30de563be63403935ab892811943ba
Trey Dott <treydott@Treys-MacBook-Pro.local>
2026-03-03 13:35:14 -0800

    adding vimtex plugin

--- init.lua
+++ init.lua
@@ -233,6 +233,15 @@ require('lazy').setup({
     lazy = false,
     opts = { auto_start = true, log_level = 'info' },
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
